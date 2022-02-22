53bd6399813408ed15a6dc03ed43be0f1adc5cc9
soumanpaul <souman1729@gmail.com>
2022-02-23 00:30:59 +0530

    83-Remove-Duplicates-from-Sorted-List.cpp

--- 203-Remove-Linked-List-Elements/203-Remove-Linked-List-Elements.cpp
+++ 203-Remove-Linked-List-Elements/203-Remove-Linked-List-Elements.cpp
@@ -1,9 +1,41 @@
+// Recursion
+
 class Solution {
 public:
     ListNode *removeElements(ListNode *head, int val) {
         if (head == NULL)
             return NULL;
         head->next = removeElements(head->next, val);
-        return head->val == val ? head->next : head;
+        return (head->val == val) ? head->next : head;
+    }
+};
+
+// Iterative
+
+class Solution {
+public:
+    ListNode *removeElements(ListNode *head, int val) {
+        ListNode *sentinel = new ListNode(0);
+        sentinel->next = head;
+
+        ListNode *prev = sentinel, *curr = head, *toDelete = nullptr;
+        while (curr != nullptr) {
+            if (curr->val == val) {
+                prev->next = curr->next;
+                toDelete = curr;
+            } else
+                prev = curr;
+
+            curr = curr->next;
+
+            if (toDelete != nullptr) {
+                delete toDelete;
+                toDelete = nullptr;
+            }
+        }
+
+        ListNode *ret = sentinel->next;
+        delete sentinel;
+        return ret;
     }
 };
