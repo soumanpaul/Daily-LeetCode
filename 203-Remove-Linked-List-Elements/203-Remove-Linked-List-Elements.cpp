e15d59490e3ecfd07bf820e270f8e2b193aadb30
soumanpaul <souman1729@gmail.com>
2022-02-22 19:33:21 +0530

    Runtime: 26 ms, faster than 65.38% of C++ online submissions for Remove Linked List Elements.

---
+++ 203-Remove-Linked-List-Elements/203-Remove-Linked-List-Elements.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    ListNode *removeElements(ListNode *head, int val) {
+        if (head == NULL)
+            return NULL;
+        head->next = removeElements(head->next, val);
+        return head->val == val ? head->next : head;
+    }
+};
