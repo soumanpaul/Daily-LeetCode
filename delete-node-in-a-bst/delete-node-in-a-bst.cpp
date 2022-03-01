4f9dc699d30ed2c401192032c7083ce77236831b
Souman Paul <souman1729@gmail.com>
2022-03-01 17:09:53 +0530

    Time: 42 ms, Memory: 32.8 MB - LeetHub

---
+++ delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* deleteNode(TreeNode* root, int key) {
+        if(root==NULL) return root;
+        
+        if (key < root->val)
+            root->left = deleteNode(root->left, key);
+        else if (key > root->val)
+            root->right = deleteNode(root->right, key);
+        else {
+            if(root->left == NULL){
+                return root->right;
+            }else if(root->right == NULL){
+                return root->left;
+            } 
+//             node with two chile
+            TreeNode* temp = successor(root->right);
+            root->val = temp->val;
+            root->right = deleteNode(root->right, temp->val);
+        }
+        return root;
+    }
+    
+    TreeNode* successor(TreeNode* root){
+        TreeNode* current = root;
+        while(current->left!=NULL) {
+            current=current->left;
+        }
+        return current;
+    }
+};
