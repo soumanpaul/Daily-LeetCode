322f704a7f08e06b8f7f2647e0f2714880b58668
Souman Paul <souman1729@gmail.com>
2022-03-02 01:06:45 +0530

    Update 338-counting-bits.cpp

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
+    
     int countSetBits(int n){
-    if (n == 0)
-        return 0;
-    else
-        // if last bit set add 1 else add 0
-        return (n & 1) + countSetBits(n >> 1);
+        if (n == 0) return 0;
+        else return (n & 1) + countSetBits(n >> 1);
     }
+    
     vector<int> countBits(int n) {
         vector<int>result;
         for(int i=0;i<=n;i++){
