fd7834a905264492d3b0e0fca640e7c95b7355aa
Souman Paul <souman1729@gmail.com>
2022-03-07 15:25:25 +0530

    Time: 4 ms, Memory: 6 MB - LeetHub

---
+++ n-th-tribonacci-number/n-th-tribonacci-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    int dp[38];
+public:
+    int tribonacci(int n) {
+        if(n<2) return n;
+        if(n==2) return 1;
+        if(!dp[n])
+            dp[n] = tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1);
+        return dp[n];
+    }
+};
