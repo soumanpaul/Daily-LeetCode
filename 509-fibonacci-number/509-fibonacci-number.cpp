184ad6403654b89f7703c9d4451df101d17a7d45
Souman Paul <souman1729@gmail.com>
2022-03-07 14:50:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.25%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -3,8 +3,9 @@ class Solution {
 public:
     
     int fib(int n) {
-        if(n==0) return 0;
-        if(n==1) return 1;
-        return fib(n-1)+fib(n-2);
+        if(n<2) return n;
+        if(!dp[n])
+            dp[n]=fib(n-1)+fib(n-2);
+        return dp[n];
     }
 };
