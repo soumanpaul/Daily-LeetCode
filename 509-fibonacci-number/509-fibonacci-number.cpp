ec24bfa256017499132f25d2987619733138b45a
Souman Paul <souman1729@gmail.com>
2022-03-07 23:52:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (40.25%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,14 +1,28 @@
+
 class Solution {
 public:
-    int fib(int n) {
-        int current = 0;
-        int prev1 = 1, prev2 = 0;
-        if(n<=1) return n;
-        for(int i = 2; i<=n; i++){
-            current = prev1+prev2;
-            prev2=prev1;
-            prev1= current;
-        }
-       return current;
+      int fib(int n) {
+        int F[2][2] = {{1, 1}, {1, 0}};
+        if (n == 0)
+            return 0;
+        power(F, n - 1);
+        return F[0][0];
+    }
+    void power(int F[2][2], int n) {
+        int M[2][2] = {{1, 1}, {1, 0}};
+
+        for (int i = 2; i <= n; i++)
+            multiply(F, M);
+    }
+    void multiply(int F[2][2], int M[2][2]) {
+        int x = F[0][0] * M[0][0] + F[0][1] * M[1][0];
+        int y = F[0][0] * M[0][1] + F[0][1] * M[1][1];
+        int z = F[1][0] * M[0][0] + F[1][1] * M[1][0];
+        int w = F[1][0] * M[1][1] + F[1][1] * M[1][1];
+
+        F[0][0] = x;
+        F[0][1] = y;
+        F[1][0] = z;
+        F[1][1] = w;
     }
 };
