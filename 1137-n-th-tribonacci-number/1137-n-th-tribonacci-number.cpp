36f7703dc4f5e867dbc492df63b71cc0ee230a7f
Souman Paul <souman1729@gmail.com>
2022-03-08 01:49:12 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (83.12%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.cpp
@@ -3,8 +3,14 @@ class Solution {
 public:
     int tribonacci(int n) {
         if(n<3) return n==0 ? 0 : 1;
-        if(!dp[n])
-            dp[n] = tribonacci(n-3)+tribonacci(n-2)+tribonacci(n-1);
-        return dp[n];
+        
+        int temp , x=0, y=1,z=1;
+            for(int i=3; i<=n; i++){
+                temp = x+y+z;
+                x = y;
+                y = z;
+                z =temp;
+            }
+        return z;
     }
 };
