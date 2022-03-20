e0bfb442d225122d629c9d376962e409b23f757b
Souman Paul <souman1729@gmail.com>
2022-03-21 02:28:24 +0530

    Time: 206 ms (39.05%), Space: 111.8 MB (10.31%) - LeetHub

--- 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
+++ 1007-minimum-domino-rotations-for-equal-row/1007-minimum-domino-rotations-for-equal-row.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    int check(int x, vector<int>& A, vector<int>& B, int n){
-        
-        int aRotation=0, bRotation=0;
-        for(int i = 0; i<n; i++){
-            if(A[i] != x && B[i] != x) return -1;
-            else if(A[i]!= x) aRotation++;
-            else if(B[i] != x) bRotation++;
-        }
-        return min(aRotation , bRotation);
-    }
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
-        int n = tops.size();
-        int rotations = check(tops[0], bottoms, tops, n);
-        
-        if(rotations !=-1 || tops[0] == bottoms[0]) return rotations;
-        else return check(bottoms[0], tops, bottoms, n);
+        int N=tops.size();
+        map<int, int> m1, m2, m3;
+        for(int i=0;i<N;i++){
+            m1[tops[i]]++;
+            m2[bottoms[i]]++;
+            if(tops[i]==bottoms[i])
+                m3[bottoms[i]]++;
+        }
+        int ans=INT_MAX;
+        for(int i=1;i<=6;i++){
+            if(m1[i]+m2[i]-m3[i]==N)
+                ans=min(ans, N-max(m1[i],m2[i]));
+        }
+        if(ans==INT_MAX)
+            return -1;
+        return ans;
     }
 };
