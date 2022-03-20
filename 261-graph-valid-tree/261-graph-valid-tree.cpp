6d107295b6e07b58aefebd8e1fc838ccc6a10ffa
Souman Paul <souman1729@gmail.com>
2022-03-20 12:27:49 +0530

    Time: 40 ms (23.33%), Space: 12.1 MB (67.83%) - LeetHub

---
+++ 261-graph-valid-tree/261-graph-valid-tree.cpp
@@ -0,0 +1,58 @@
+class UnionFind{
+    private:
+        vector<int> root;
+        vector<int> rank;
+        int count;
+    public:
+        UnionFind(int sz): root(sz), rank(sz){
+            for(int i =0; i < sz; i++){
+                root[i] = i;
+                rank[i] = i;
+            }
+            count = sz;
+        }
+        void Union(int x, int y){
+            int rootX = find(x);
+            int rootY = find(y);
+            if(rootX != rootY){
+                if(rank[rootX] > rank[rootY])
+                    root[rootY] = rootX;
+                else if(rank[rootX] < rank[rootY])
+                    root[rootX] = rootY;
+                else {
+                    root[rootY] = rootX;
+                    rank[rootX] +=1; 
+                     }
+                count--;
+            }
+        }
+        int find(int x) {
+            if(x == root[x])
+                return x;
+            root[x] = find(root[x]);
+            return root[x];
+        }
+        bool isConnected(int x, int y) {
+            return find(x) == find(y);
+        }
+        int getCount(){
+            return count;
+        }
+};
+
+class Solution {
+public:
+    bool validTree(int n, vector<vector<int>>& edges) {
+         UnionFind uf(n);
+         for(int i=0; i< edges.size(); i++){
+            int x = uf.find(edges[i][0]);
+            int y = uf.find(edges[i][1]);
+            if (x == y) {
+                return false;
+            } else {
+               uf.Union(edges[i][0],edges[i][1]);
+            } 
+         } 
+        return true && uf.getCount()==1;
+    }
+};
