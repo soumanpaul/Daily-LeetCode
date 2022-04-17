cff8c81dd530c0b02caf80449aa9105c84ee97d2
Souman Paul <souman1729@gmail.com>
2022-04-17 14:15:38 +0530

    Time: 0 ms (100.00%), Space: 8 MB (19.70%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -13,13 +13,12 @@ class Solution {
        
 public:
     void inorder(TreeNode* root, vector<int>& vec){
-         if(root!=NULL){
+         if(root==NULL)  return ;
             inorder(root->left,vec);
             vec.push_back(root->val);
             inorder(root->right,vec);
-        }
-        return ;
     }
+    
     TreeNode* increasingBST(TreeNode* root) {
         vector<int> vec;
         inorder(root,vec);
