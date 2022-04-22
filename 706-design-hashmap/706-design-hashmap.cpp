04f116e7ccf0884fe40996574d4f5f466095bb62
Souman Paul <souman1729@gmail.com>
2022-04-22 15:55:01 +0530

    Time: 184 ms (56.11%), Space: 208.2 MB (14.49%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.cpp
+++ 706-design-hashmap/706-design-hashmap.cpp
@@ -1,58 +1,16 @@
 class MyHashMap {
-    int prime = 2069;
-    vector<list<pair<int,int>>> hash;
 public:
+    int data[1000001];
     MyHashMap() {
-        list<pair<int,int>> values;
-        for(int i=0; i< prime; i++)
-            hash.push_back(values);
+        fill(data, data + 1000000, -1);
     }
-    
-    void put(int key, int value) {
-        int hash_key = key % prime;
-        list<pair<int,int>> val = hash[hash_key];
-        val.push_front({key,value});
-        hash[hash_key] = val;
+    void put(int key, int val) {
+        data[key] = val;
     }
-    
     int get(int key) {
-        int hash_key = key % prime;
-        list<pair<int,int>> val = hash[hash_key];
-        for(auto x : val){
-            if(x.first==key)
-                return x.second;
-        }
-        return -1;
+        return data[key];
     }
-    
-    pair<int,int> find(list<pair<int,int>> &val, int key) {
-        pair<int, int> ans = {INT_MAX, INT_MAX};
-        for(auto x : val) {
-            if(x.first == key)
-                ans = x;
-        }
-        return ans;
-    }
-    
     void remove(int key) {
-        int hash_key = key % prime;
-        list<pair<int,int>> val = hash[hash_key];
-        if(val.empty()) return;
-        while(!val.empty()){
-            pair<int,int> data = find(val, key);
-            if(data.first == INT_MAX && data.second==INT_MAX)
-                break;
-            val.remove(data);
-        }
-        hash[hash_key] = val;
+        data[key] = -1;
     }
-    
 };
-
-/**
- * Your MyHashMap object will be instantiated and called as such:
- * MyHashMap* obj = new MyHashMap();
- * obj->put(key,value);
- * int param_2 = obj->get(key);
- * obj->remove(key);
- */
