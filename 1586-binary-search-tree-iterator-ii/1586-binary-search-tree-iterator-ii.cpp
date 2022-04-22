e61b7155cf54c6ffd9a83aeff7a29339045b7dfc
Souman Paul <souman1729@gmail.com>
2022-04-22 17:36:53 +0530

    Time: 517 ms (14.70%), Space: 150.1 MB (63.17%) - LeetHub

--- 1586-binary-search-tree-iterator-ii/1586-binary-search-tree-iterator-ii.cpp
+++ 1586-binary-search-tree-iterator-ii/1586-binary-search-tree-iterator-ii.cpp
@@ -9,41 +9,36 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class BSTIterator {
-    int n;
-    int pointer;
-    vector<int> nodes;
-public:
-    void inorder(TreeNode* root){
-        if(root==NULL) return;
-        inorder(root->left);
-        nodes.push_back(root->val);
-        inorder(root->right);
-    }
-    BSTIterator(TreeNode* root) {
-        inorder(root);
-        n = nodes.size();
-        pointer = -1;
-    }
-    
-    int next() {
-        ++pointer;
-        return  nodes[pointer];
-    }
-    
-    bool hasNext() {
-        return pointer < n-1;
-    }
-    
-    bool hasPrev() {
-        return pointer > 0;
-    }
-    
-    int prev() {
-        --pointer;
-        return nodes[pointer];
-    }
+    stack<TreeNode*> path;
+    vector<int> vals;
+    int pos = -1;
+    public:
+   
+        void traverseLeft(TreeNode *root) {
+            for (; root != nullptr; root = root->left)
+	            path.push(root);
+        }
+        
+        BSTIterator(TreeNode* root) { traverseLeft(root); }
+        
+        bool hasNext() { return pos + 1 < vals.size() || !path.empty(); }
+        
+        int next() {
+            if (++pos == vals.size()) {
+                auto cur = path.top();
+                path.pop();
+                traverseLeft(cur->right);
+                vals.push_back(cur->val);
+        }
+    return vals[pos];
+}
+bool hasPrev() { return pos > 0; }
+int prev() { return vals[--pos]; }
+
 };
+
 
 /**
  * Your BSTIterator object will be instantiated and called as such:
