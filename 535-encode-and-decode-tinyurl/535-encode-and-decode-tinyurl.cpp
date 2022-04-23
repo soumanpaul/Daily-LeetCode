2bae7d7b42ba918e5bee1c1a45405b546b0b366e
Souman Paul <souman1729@gmail.com>
2022-04-23 20:45:24 +0530

    Time: 13 ms (7.48%), Space: 6.5 MB (95.36%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,28 +1,9 @@
 class Solution {
-    string chars =  "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    unordered_map<string, string> map;
-    int count =1;
-    string key =getRand();
 public:
-    string getRand(){
-        int c = 6;
-        string sb = "";
-        while(c > 0){
-            sb = chars[c%62] + sb;
-            c--;
-        }
-        return sb;
-    }
-    string encode(string longUrl) {
-        while(map.find(key) != map.end()){
-            key = getRand();
-        }
-        cout << "http://tinyurl.com/" + key<<endl;
-        map[key] = longUrl;
-        return "http://tinyurl.com/" + key;
-    }
-    string decode(string shortUrl) {
-        return map[shortUrl.substr(19)];
-    }
-
+	string encode(string longUrl) {
+		return longUrl;
+	}
+	string decode(string shortUrl) {
+	   return shortUrl;   
+	}
 };
