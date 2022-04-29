0fdc12f50d4874eada83e4502ca69c68e0d2e61c
Souman Paul <souman1729@gmail.com>
2022-04-29 16:03:42 +0530

    Time: 267 ms, Memory: 57.6 MB - LeetHub

--- min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,49 +1,10 @@
-class UnionFind {
-public:
-    UnionFind(int sz) : root(sz), rank(sz) {
-        for (int i = 0; i < sz; i++) {
-            root[i] = i;
-            rank[i] = 1;
-        }
-    }
-
-    int find(int x) {
-        if (x == root[x]) {
-            return x;
-        }
-        return root[x] = find(root[x]);
-    }
-
-    void unionSet(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
-            }
-        }
-    }
-
-    bool connected(int x, int y) {
-        return find(x) == find(y);
-    }
-
-private:
-    vector<int> root;
-    vector<int> rank;
-};
-
-
-
 class Edge {
-    public:
-    int src, dest, cost;
-    Edge(int s1, int s2, int w): src(s1),dest(s2), cost(w){}
+public:
+    int point1;
+    int point2;
+    int cost;
+    Edge(int point1, int point2, int cost)
+        : point1(point1), point2(point2), cost(cost) {}
 };
 // Overload the < operator.
 bool operator<(const Edge &edge1, const Edge &edge2) {
@@ -52,43 +13,47 @@ bool operator<(const Edge &edge1, const Edge &edge2) {
 
 class Solution {
 public:
-
-    int minCostConnectPoints(vector<vector<int>>& points) {
+    // Prim's algorithm
+    int minCostConnectPoints(vector<vector<int>> &points) {
         if (points.size() == 0) {
             return 0;
         }
-        int n = points.size();
-        UnionFind uf(n);
-        vector<vector<int>>allEdges;
+        int sz = points.size();
         priority_queue<Edge> pq;
-        for(int i=0; i<n;i++){
-            vector<int> &cord1 = points[i];
-            for(int j=i+1;j<n;j++){
-                vector<int> &cord2 = points[j];
-                int weight = abs(cord1[0]-cord2[0])+abs(cord1[1]-cord2[1]);
-                Edge edge(i,j,weight);
-                pq.push(edge);
-            }
-        }
-        // Storing all edges of our complete graph.
+        vector<bool> visited(sz);
         int result = 0;
-        int count = n - 1;
-        
-        while(!pq.empty() && count > 0){
+        int count = sz - 1;
+        // Add all edges from points[0] vertex
+        vector<int> &coordinate1 = points[0];
+        for (int j = 1; j < sz; j++) {
+            // Calculate the distance between two coordinates
+            vector<int> &coordinate2 = points[j];
+            int cost = abs(coordinate1[0] - coordinate2[0]) +
+                       abs(coordinate1[1] - coordinate2[1]);
+            Edge edge(0, j, cost);
+            pq.push(edge);
+        }
+        visited[0] = true;
+
+        while (!pq.empty() && count > 0) {
             Edge edge = pq.top();
             pq.pop();
-            if(!uf.connected(edge.src, edge.dest)) {
-                uf.unionSet(edge.src, edge.dest);
-                result += edge.cost;
+            int point1 = edge.point1;
+            int point2 = edge.point2;
+            int cost = edge.cost;
+            if (!visited[point2]) {
+                result += cost;
+                visited[point2] = true;
+                for (int j = 0; j < sz; j++) {
+                    if (!visited[j]) {
+                        int distance = abs(points[point2][0] - points[j][0]) +
+                                       abs(points[point2][1] - points[j][1]);
+                        pq.push(Edge(point2, j, distance));
+                    }
+                }
                 count--;
             }
         }
-
-  
-    return result;
+        return result;
     }
 };
-
-
-
-
