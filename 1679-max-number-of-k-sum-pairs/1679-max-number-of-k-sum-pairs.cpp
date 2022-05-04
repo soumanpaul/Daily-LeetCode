d583669279016ce6671d2765e0e08f78690d1022
Souman Paul <souman1729@gmail.com>
2022-05-04 12:40:35 +0530

    Time: 213 ms (40.45%), Space: 72.9 MB (26.85%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,22 +1,16 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int, int> map;
-        int count = 0;
-        // build the hashmap with count of occurence of every element in array
-        for (int i = 0; i < nums.size(); i++) {
-            map[nums[i]] = map[nums[i]] + 1;
-        }
-        for (int i = 0; i < nums.size(); i++) {
-            int current = nums[i];
-            int complement = k - nums[i];
-            if (map[current] > 0 && map[complement] > 0) {
-                if ((current == complement) && map[current] < 2) continue;
-                map[current] = map[current] - 1;
-                map[complement] = map[complement] - 1;
+        int count=0;
+        unordered_map<int,int> map;
+        for(int i=0; i< nums.size();i++){
+            int item = k-nums[i]; 
+            if(map[item]>0){
+                map[item]-=1;
                 count++;
-            }
-        }
+            }else 
+                map[nums[i]]+=1; 
+        }   
         return count;
     }
 };
