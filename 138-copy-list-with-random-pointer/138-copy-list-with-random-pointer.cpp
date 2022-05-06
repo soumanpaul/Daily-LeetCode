3e24d7b58718b03535a0791e28114d61bd79995c
Souman Paul <souman1729@gmail.com>
2022-05-06 19:17:42 +0530

    Time: 15 ms (36.40%), Space: 11.3 MB (63.73%) - LeetHub

--- 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.cpp
@@ -1,36 +1,59 @@
-class Solution {
+/*
+// Definition for a Node.
+class Node {
 public:
-    map<Node *, Node *> mp;
-
-    Node *copyRandomList(Node *head) {
-
-        if (head == NULL)
-            return NULL;
-        Node *current = new Node(head->val);
-        Node *newHead = current;
-        Node *oldHead = head;
-
-        mp[head] = current;
-        while (head != NULL) {
-            Node *newNext = NULL;
-            if (head->next != NULL) {
-                newNext = new Node(head->next->val);
-                mp[head->next] = newNext;
-            }
-
-            current->next = newNext;
-            current = current->next;
-            head = head->next;
-        }
-
-        Node *newNewHead = newHead;
-
-        while (oldHead != NULL) {
-            newHead->random = oldHead->random == NULL ? NULL : mp[oldHead->random];
-            oldHead = oldHead->next;
-            newHead = newHead->next;
-        }
-
-        return newNewHead;
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
     }
 };
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        
+        if(head==NULL) return NULL;        
+        Node *next, *temp, *newPt;
+        for(temp=head; temp!=NULL;){
+            next=temp->next;
+            temp->next=new Node(temp->val);
+            temp->next->next=next;
+            temp=next;
+        }
+        for(Node *curr=head; curr!=NULL; curr=curr->next->next){
+            curr->next->random= curr->random!=NULL ? curr->random->next : NULL;  
+        }
+        Node *original=head, *copy = head->next;
+        newPt=copy;
+        
+        while(original && copy){
+            original->next = original->next ? original->next->next : original->next;
+            copy->next = copy->next ? copy->next->next : copy->next;
+            original=original->next;
+            copy=copy->next;
+        }
+        return newPt;
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
