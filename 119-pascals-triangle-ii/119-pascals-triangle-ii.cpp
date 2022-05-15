e92c3161e6016a0aff592c82c28a7c903c13502c
Souman Paul <souman1729@gmail.com>
2022-05-15 15:00:28 +0530

    Time: 3 ms (40.65%), Space: 6.4 MB (52.50%) - LeetHub

--- 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
+++ 119-pascals-triangle-ii/119-pascals-triangle-ii.cpp
@@ -1,24 +1,12 @@
 class Solution {
-    int memo[1000][1000];
-public:
-     
-    int getTri(int rowIndex, int colIndex){
-        if(memo[rowIndex][colIndex]){
-            return memo[rowIndex][colIndex];
-        }
-        if(colIndex==0 || rowIndex == colIndex)
-            return 1;
-        memo[rowIndex][colIndex] = getTri(rowIndex-1,colIndex-1) + getTri(rowIndex-1,colIndex);
-         return memo[rowIndex][colIndex];
-    }
-    
-    vector<int> getRow(int rowIndex) {
-        vector<int> ans;
-        for(int i=0; i<= rowIndex; i++){
-            
-            ans.push_back(getTri(rowIndex,i));
-        }
-        return ans;
-    }
-    
+ public:
+  vector<int> getRow(int rowIndex) {
+    vector<int> ans = vector<int>(rowIndex + 1, 1);
+
+    for (int i = 1; i < rowIndex; i++)
+      for (int j = i; j > 0; j--)
+        ans[j] += ans[j - 1];  // ans[j] = ans[j-1] + ans[j]
+
+    return ans;
+  }
 };
