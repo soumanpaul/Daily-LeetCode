eab39ddbf4a67adc8a80cd1fa407083659f72c9c
Souman Paul <souman1729@gmail.com>
2022-06-01 13:44:41 +0530

    Time: 62 ms (93.48%), Space: 19 MB (91.30%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,10 +1,23 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        unordered_set<string> us;
-        for (int i = 0; i + k <= s.size(); i++){
-            us.insert(s.substr(i, k));
+        const int powK= 1 << k;
+        int need = powK;
+        vector<bool> found(need, false);
+        int curr =0;
+        
+        for(int i=0; i <s.size(); i++){
+            curr <<=1;
+            curr&=~powK;
+            if(s[i]=='1') curr |=1;
+    
+            if(i >=k-1 && !found[curr]){
+                found[curr] = true;
+                need--;
+                if(need==0)
+                    return true;
+            }
         }
-        return us.size() == 1 << k;
+        return false;
     }
 };
