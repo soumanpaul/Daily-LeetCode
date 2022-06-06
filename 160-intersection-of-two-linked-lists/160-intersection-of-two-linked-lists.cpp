896a173902eb8663915b93a73306a737afb07a45
Souman Paul <souman1729@gmail.com>
2022-06-06 07:00:55 +0530

    Time: 52 ms (75.65%), Space: 14.5 MB (88.39%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -8,21 +8,44 @@
  */
 class Solution {
 public:
-    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_set<ListNode*> nodeAddress;
-        ListNode* la=headA, *lb=headB;
-        
-        while(lb!=NULL){
-            nodeAddress.insert(lb);
-            lb=lb->next;
+    int getCount(ListNode *head){
+        ListNode * temp = head;
+        int count=0;
+        while(temp!=NULL){
+            count++;
+            temp=temp->next;
         }
-        while(la!=NULL){
-            if(nodeAddress.find(la)!=nodeAddress.end()){
-                return la;
-            }
-            nodeAddress.insert(la);
-            la=la->next;
+        return count;
+    }
+    ListNode* intersectionNode(int d , ListNode* la, ListNode *lb){
+        ListNode *curr1 = la;
+        ListNode *curr2 = lb;
+        for(int i=0; i<d;i++){
+            if(curr1==NULL)
+                return NULL;
+            curr1=curr1->next;
+        }
+        while(curr1!=NULL && curr2!=NULL){
+            if(curr1==curr2)
+                return curr1;
+            curr1=curr1->next;
+            curr2=curr2->next;
         }
         return NULL;
+    }
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode *la =headA, *lb=headB;
+        
+        int l1=getCount(headA);
+        int l2=getCount(headB);
+        
+        if(l1 > l2){
+            int d = l1-l2;
+            return intersectionNode(d, la, lb);
+        }else{
+            int d = l2-l1;
+            return intersectionNode(d, lb, la);
+        }
+        // return NULL;
     }
 };
