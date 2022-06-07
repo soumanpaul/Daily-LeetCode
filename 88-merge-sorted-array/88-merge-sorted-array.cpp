8d84ae3685465e2d140427ee0a81519d036aba79
Souman Paul <souman1729@gmail.com>
2022-06-08 03:29:47 +0530

    Time: 0 ms (100.00%), Space: 9.1 MB (72.54%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
     void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
-        if (m == 0) {
+        
+        if (m == 0  ) {
             nums1 = nums2;
             return;
         }
         if (n == 0)
             return;
-        int L[m], R[n];
+        
+        int nums1_copy[m];
 
         for (int i = 0; i < m; i++)
-            L[i] = nums1[i];
-        for (int j = 0; j < n; j++)
-            R[j] = nums2[j];
-
-        int k = 0;
-        int i = 0, j = 0;
-
-        while (i < m && j < n){
-            if (L[i] <= R[j]) {
-                nums1[k] = L[i];
-                i++;
-            } else {
-                nums1[k] = R[j];
-                j++;
+            nums1_copy[i] = nums1[i];
+        
+        int p1=0;
+        int p2=0;
+        
+        for(int i=0; i < n+m; i++){
+            if(p2>=n || (p1 < m && nums1_copy[p1] < nums2[p2])){
+                nums1[i]=nums1_copy[p1++];
+            }else {
+                nums1[i] = nums2[p2++];
             }
-            k++;
-        }
-        while (i < m) {
-            nums1[k] = L[i];
-            i++;
-            k++;
-        }
-        while (j < n) {
-            nums1[k] = R[j];
-            j++;
-            k++;
-        }
+        }    
     }
 };
